04fa761324e2567cec83a28c88fc3b7382b8a88e
Kenrick Zhou <Kenrick.Zhou@gmail.com>
2013-06-19 20:23:43 +0800

    init commit

---
+++ Rakefile
@@ -0,0 +1,7 @@
+#!/usr/bin/env rake
+# Add your own tasks in files placed in lib/tasks ending in .rake,
+# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
+
+require File.expand_path('../config/application', __FILE__)
+
+Case4rom::Application.load_tasks
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Case4rom::Application.config.secret_token = '46a934df86eb9b1cd51e1de8082cc7b1a1c3812d65be920420035df5f547ee65321463e766e0185694f3a331ab3abb4217987a3cfe450795ec6a853bce239f9e'
