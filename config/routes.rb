7800b105ce218bf49e564d9dfa922fc15952205a
yuhaijun <yu_hai_jun@163.com>
2013-06-24 10:27:20 +0800

    修改signUp

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Case4rom::Application.routes.draw do
   root :to => 'users#index'
   get 'users/login' => 'users#login'
   get 'users/create' => 'users#create'
-  get 'users/signup' => 'users#signUp'
+  get 'users/signUp' => 'users#signUp'
   match ':controller(/:action(/:id))(.:format)'
   resources :users
 end
