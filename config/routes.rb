c988fd45ea6996c7e6143f7c9c6405d5e51360cc
Wangqi <qwang001@kaikeba.com>
2013-06-26 16:43:48 +0800

    修改密码

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Case4rom::Application.routes.draw do
   root :to => 'users#index'
-  get 'users/login' => 'users#login'
-  post 'users/create'
-  post 'users/signUp' => 'users#signUp'
-  post 'users/forgot_password' => 'users#forgot_password'
+  #get 'users/login' => 'users#login'
+  #post 'users/signUp' => 'users#signUp'
   match ':controller(/:action(/:id))(.:format)'
   resources :users
 end
