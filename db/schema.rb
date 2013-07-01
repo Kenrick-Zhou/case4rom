aa0c6ae89bdc5d79bcebc3d34377ffbcd0443f52
yuhaijun <yu_hai_jun@163.com>
2013-07-01 14:42:43 +0800

    update db

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130619082806) do
+ActiveRecord::Schema.define(:version => 20130701055315) do
 
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "password"
     t.string   "password_confirm"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
+    t.string   "email",            :limit => 25
   end
 
 end
